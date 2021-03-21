8d9f365b5821ed56233c3551107d1867cfd4d2b2
Paulina Badura <P2574096@my365.dmu.ac.uk>
2021-03-21 19:56:53 +0000

    New project

---
+++ APTeamFootball/APTeamFootball/Startup.cs
@@ -0,0 +1,14 @@
+﻿using Microsoft.Owin;
+using Owin;
+
+[assembly: OwinStartupAttribute(typeof(APTeamFootball.Startup))]
+namespace APTeamFootball
+{
+    public partial class Startup
+    {
+        public void Configuration(IAppBuilder app)
+        {
+            ConfigureAuth(app);
+        }
+    }
+}

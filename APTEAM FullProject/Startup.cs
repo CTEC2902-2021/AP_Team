13730b6ecfe1af3248593f88996f801e776ccfc0
Sofia Kirsnerova <P2539038@my365.dmu.ac.uk>
2021-03-21 19:43:26 +0000

    Add project files.

---
+++ APTEAM FullProject/Startup.cs
@@ -0,0 +1,14 @@
+﻿using Microsoft.Owin;
+using Owin;
+
+[assembly: OwinStartupAttribute(typeof(APTEAM_FullProject.Startup))]
+namespace APTEAM_FullProject
+{
+    public partial class Startup
+    {
+        public void Configuration(IAppBuilder app)
+        {
+            ConfigureAuth(app);
+        }
+    }
+}

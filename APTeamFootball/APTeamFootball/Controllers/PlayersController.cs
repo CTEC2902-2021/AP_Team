2222971ffa019dea95efe7e5f41477a771c53488
Sofia Kirsnerova <P2539038@my365.dmu.ac.uk>
2021-04-27 02:21:49 +0100

    Added Players Classes + scaffolding + added player to test if it works

---
+++ APTeamFootball/APTeamFootball/Controllers/PlayersController.cs
@@ -0,0 +1,128 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Data;
+using System.Data.Entity;
+using System.Linq;
+using System.Net;
+using System.Web;
+using System.Web.Mvc;
+using APTeamFootball.Models;
+
+namespace APTeamFootball.Controllers
+{
+    public class PlayersController : Controller
+    {
+        private PlayersContext db = new PlayersContext();
+
+        // GET: Players
+        public ActionResult Index()
+        {
+            return View(db.Players.ToList());
+        }
+
+        // GET: Players/Details/5
+        public ActionResult Details(int? id)
+        {
+            if (id == null)
+            {
+                return new HttpStatusCodeResult(HttpStatusCode.BadRequest);
+            }
+            Players players = db.Players.Find(id);
+            if (players == null)
+            {
+                return HttpNotFound();
+            }
+            return View(players);
+        }
+
+        // GET: Players/Create
+        public ActionResult Create()
+        {
+            return View();
+        }
+
+        // POST: Players/Create
+        // To protect from overposting attacks, please enable the specific properties you want to bind to, for 
+        // more details see https://go.microsoft.com/fwlink/?LinkId=317598.
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public ActionResult Create([Bind(Include = "PlayersId,Appearances,Comments,DOB,Goals,Name,Number,Position")] Players players)
+        {
+            if (ModelState.IsValid)
+            {
+                db.Players.Add(players);
+                db.SaveChanges();
+                return RedirectToAction("Index");
+            }
+
+            return View(players);
+        }
+
+        // GET: Players/Edit/5
+        public ActionResult Edit(int? id)
+        {
+            if (id == null)
+            {
+                return new HttpStatusCodeResult(HttpStatusCode.BadRequest);
+            }
+            Players players = db.Players.Find(id);
+            if (players == null)
+            {
+                return HttpNotFound();
+            }
+
+            return View(players);
+        }
+
+        // POST: Players/Edit/5
+        // To protect from overposting attacks, please enable the specific properties you want to bind to, for 
+        // more details see https://go.microsoft.com/fwlink/?LinkId=317598.
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public ActionResult Edit([Bind(Include = "PlayersId,Appearances,Comments,DOB,Goals,Name,Number,Position")] Players players)
+        {
+            if (ModelState.IsValid)
+            {
+                db.Entry(players).State = EntityState.Modified;
+                db.SaveChanges();
+                return RedirectToAction("Index");
+            }
+            return View(players);
+        }
+
+        // GET: Players/Delete/5
+        public ActionResult Delete(int? id)
+        {
+            if (id == null)
+            {
+                return new HttpStatusCodeResult(HttpStatusCode.BadRequest);
+            }
+            Players players = db.Players.Find(id);
+            if (players == null)
+            {
+                return HttpNotFound();
+            }
+            return View(players);
+        }
+
+        // POST: Players/Delete/5
+        [HttpPost, ActionName("Delete")]
+        [ValidateAntiForgeryToken]
+        public ActionResult DeleteConfirmed(int id)
+        {
+            Players players = db.Players.Find(id);
+            db.Players.Remove(players);
+            db.SaveChanges();
+            return RedirectToAction("Index");
+        }
+
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing)
+            {
+                db.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+    }
+}
